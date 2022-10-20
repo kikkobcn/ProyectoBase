2290a8c847d03640adf8d8e962e7b450f6335ce0
errmake <make9m9@gmail.com>
2022-10-20 19:51:29 +0200

    Navbar improved

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,34 @@
-export const Header = () =>{
-    return <div>
-        Header commit 1 Javi
-    </div>
+import Logo from '../assets/logo.png';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export const Header = () => {
+    return (
+        <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={Logo}
+              width="60"
+              height="60"
+              className="d-inline-block align-middle"
+            />{' '}
+            Proyecto Ethereum
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+            <Nav.Link href="#Company">Company</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      </>
+    )
 }
