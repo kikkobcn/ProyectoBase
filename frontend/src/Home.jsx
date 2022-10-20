6931e7691b0cc81b5f41912861a3b385040bb90b
kikkobcn <87577869+kikkobcn@users.noreply.github.com>
2022-10-20 18:19:05 +0200

    Actualizado footer con iconos redes sociales

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,11 +2,13 @@ import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
 import { Outlet } from "react-router-dom"
 export const Home = () =>{
-    return <div className="container">
-        <Header></Header>
-        <div>
+    return (
+    <div className="container d-flex flex-column justify-content-between todo">
+        <Header>Header</Header>
+        <div className="flex-grow-1">
             <Outlet></Outlet>
         </div>
-        <Footer></Footer>
-    </div>
+        <Footer>Footer</Footer>
+        </div>
+    )
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,18 @@
 export const Footer = () =>{
-    return <div>
-        Footer
+
+  return  <div>
+
+<footer className="bg-light text-center text-white">
+  <div className="container p-4 pb-0">
+      <div className="sticky-md-top"><a href="">quienes somos </a></div>
+      <div className="sticky-md-top"><a href="">privacidad</a></div>
+      <div className="sticky-md-top"><a href="">Terminos y condiciones</a></div>
+      <a href="https://facebook.com" className="btn btn-primary btn-sm"  role="button" aria-disabled="true"><i className="bi bi-facebook"></i></a>
+        <a href="https://instagram.com" className="btn btn-primary btn-sm" role="button" aria-disabled="true"><i className="bi bi-instagram"></i></a>
+        <a href="https://twitter.com" className="btn btn-primary btn-sm"  role="button" aria-disabled="true"><i className="bi bi-twitter"></i></a>
+      
+  </div>
+  
+</footer>
     </div>
 }
