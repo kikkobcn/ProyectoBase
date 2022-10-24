34477c27c867b715541847c419e363d2a39429cc
errmake <make9m9@gmail.com>
2022-10-24 22:36:32 +0200

    footer organized

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,39 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export const Footer = () =>{
 
-  return  <div>
-
-<footer className="bg-light text-center text-white">
-  <div className="container p-4 pb-0">
-      <div className="sticky-md-top"><a href="">quienes somos </a></div>
-      <div className="sticky-md-top"><a href="">privacidad</a></div>
-      <div className="sticky-md-top"><a href="">Terminos y condiciones</a></div>
-      <a href="https://facebook.com" className="btn btn-primary btn-sm"  role="button" aria-disabled="true"><i className="bi bi-facebook"></i></a>
-        <a href="https://instagram.com" className="btn btn-primary btn-sm" role="button" aria-disabled="true"><i className="bi bi-instagram"></i></a>
-        <a href="https://twitter.com" className="btn btn-primary btn-sm"  role="button" aria-disabled="true"><i className="bi bi-twitter"></i></a>
-      
-  </div>
-  
-</footer>
-    </div>
+  return (
+<Container className='bg-dark'>
+  <Row className='mt-4'>
+    <Col className='col-footer'>
+      <a href="https://facebook.com" className="btn rrss btn-sm text-light"  role="button" aria-disabled="true"><i className="bi bi-facebook"></i></a>
+      <a href="https://instagram.com" className="btn rrss btn-sm text-light" role="button" aria-disabled="true"><i className="bi bi-instagram"></i></a>
+      <a href="https://twitter.com" className="btn rrss btn-sm text-light"  role="button" aria-disabled="true"><i className="bi bi-twitter"></i></a>
+    </Col>
+    <Col className='col-footer'>
+      <div>
+        <a href="" className='text-light'>Quienes Somos </a>
+      </div>
+    </Col>
+    <Col className='col-footer'>
+      <div>
+        <a href="" className='text-light'>Privacidad</a>
+        </div>
+    </Col>
+    <Col className='col-footer'>
+      <div>
+        <a href="" className='text-light'>Términos y Condiciones</a>
+        </div>
+    </Col>
+    
+  </Row>
+  <Row>
+    <Col>1 of 3</Col>
+    <Col>2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row>
+</Container>
+  )
 }
