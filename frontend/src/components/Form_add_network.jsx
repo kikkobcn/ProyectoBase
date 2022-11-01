1a18b8285a588113e002cd99ab18cd11f9354b51
María Carbajal <mcarbmont89@users.noreply.github.com>
2022-11-01 15:14:20 +0100

    se obtienen network y node pero no pasan a la URL

--- frontend/src/components/Form_add_network.jsx
+++ frontend/src/components/Form_add_network.jsx
@@ -1,13 +1,38 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
 
 export function Form_add_network() {
+  const [network, setNetwork] = useState("");
+  const [node, setNode] = useState("");
   return (
-    <form action="http://localhost:3333/network/create/7777/1" method="post">
+    <form action="http://localhost:3333/network/create/" method="post">
+      <label>
+        Enter network name:
+        <input
+          type="text"
+          value={network}
+          onChange={(e) => setNetwork(e.target.value)}
+        />
+      </label>
+      <label>
+        Enter node number:
+        <input
+          type="text"
+          value={node}
+          onChange={(e) => setNode(e.target.value)}
+        />
+      </label>
       <div>
-        <button>Send</button>
+        <button> Send Now</button>
       </div>
     </form>
+    // <form action="http://localhost:3333/network/create/7777/1" method="post">
+    //   <div>
+    //     <button>Send</button>
+    //   </div>
+    // </form>
   );
 }
 
