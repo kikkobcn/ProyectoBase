f91e7c49c32428e03957259cbf3e3a69c40b3106
errmake <make9m9@gmail.com>
2022-11-03 13:57:43 +0100

    front improvals

--- frontend/src/components/Form_add_network.jsx
+++ frontend/src/components/Form_add_network.jsx
@@ -30,51 +30,67 @@ export const Form_add_network = () => {
   const submit = (data) => {
     mutation.mutate(data);
   };
-  return (<div>
-    {mensaje != "" ? <p className="alert alert-danger">{mensaje}</p>:""}
+  return (<div className="container text-light">
+    <div className='network-container bg-dark mt-2 p-3'>
       <form onSubmit={handleSubmit((data) => submit(data))}>
-        <div>
-          <label>Network number</label>
+        <div className="form-group mb-3">
+          <label for="net_num">Network number</label>
           <input
+            type="number"
+            id="net_num"
             className="form-control"
             defaultValue="1989"
             {...register("network", { required: true })}
           />
         </div>
-        <div>
-          <label>Node Number</label>
+        <div className="form-group mb-3">
+          <label for="node_num">Node Number</label>
           <input
+            type="number"
+            id="node_num"
             className="form-control"
             defaultValue="1"
             {...register("node", { required: true })}
           />
         </div>
-        <div>
-          <label>Metamask account</label>
+        <div className="form-group mb-3">
+          <label for="met_acc">Metamask account</label>
           <input
+            type="text"
+            id="met_acc"
             className="form-control"
             defaultValue="0bD7e56e2216450828E127ad2235B77e52C0fcb2"
             {...register("metamaskid", { required: true })}
           />
         </div>
-        <div>
-          <label>Metamask Port</label>
+        <div className="form-group mb-3">
+          <label for="met_port">Metamask Port</label>
           <input
+            type="number"
+            id="met_port"
             className="form-control"
             defaultValue="9000"
             {...register("metamaskport", { required: true })}
           />
         </div>
-        <div>
-          <label>Chain ID</label>
+        <div className="form-group mb-3">
+          <label for="chain_id">Chain ID</label>
           <input
+            type="number"
+            id="chain_id"
             className="form-control"
             defaultValue="10000"
             {...register("chainID", { required: true })}
           />
         </div>
-        <input type="submit" value="enviar" />
+        <input className="btn btn-primary my-2" type="submit" value="Crear red" />
       </form>
+      {mensaje != "" ? 
+      <p className="alert alert-success">
+        Red creada correctamente!!! Datos: {mensaje}
+        </p>
+        :""}
+      </div>
     </div>
   );
 };
--- frontend/src/components/NetworksBody.jsx
+++ frontend/src/components/NetworksBody.jsx
@@ -24,34 +24,36 @@ if (error) {
 return <>{JSON.stringify(error)}</>
 }
 return (
-    <div className="container text-light">
-        <div className='text-end'>
-            <p><button type="button" className="btn btn-light btn-sm m-2">Add Network</button></p>
-        </div>   
-        <Table borderless className='text-left' id='networks-table'>
-            <thead>
-                <tr>
-                    <th className='col-md-3'>Cod. Red</th>
-                    <th className='col-md-3'>ChainID</th>
-                    <th className='col-md-3'>Cuenta</th>
-                    <th className='col-md-1'>Info</th>
-                    <th className='col-md-1'>Borrar</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    data.map((item,index) => <tr key={index}>
-                        <td >{item.numero.slice(3,item.numero.length)}</td>
-                        <td>{item.chainId}</td>
-                        <td>{item.cuentas.map((cuenta, index2) => <div>{cuenta}</div>)}</td>
-                        <td><ModalInfo nuRed= {item.numero.slice(3,item.numero.length)} idChain = {item.chainId} /></td>
-                        {/* <td><ModalDelete></ModalDelete></td> */}
-                    </tr>)
-                }
-            </tbody>
-        </Table>
+    <div className="container text-light ">
+            <div className='text-end'>
+                <p><button type="button" className="btn btn-light btn-sm m-2">Add Network</button></p>
+            </div>
+            <div className='network-container bg-dark p-3'>
+            <Table borderless className='text-left' id='networks-table'>
+                <thead>
+                    <tr>
+                        <th className='col-md-3'>Cod. Red</th>
+                        <th className='col-md-3'>ChainID</th>
+                        <th className='col-md-3'>Cuenta</th>
+                        <th className='col-md-1'>Info</th>
+                        <th className='col-md-1'>Borrar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((item, index) => <tr key={index}>
+                            <td >{item.numero.slice(3, item.numero.length)}</td>
+                            <td>{item.chainId}</td>
+                            <td>{item.cuentas.map((cuenta, index2) => <div>{cuenta}</div>)}</td>
+                            <td><ModalInfo nuRed={item.numero.slice(3, item.numero.length)} idChain={item.chainId} /></td>
+                            <td><ModalDelete></ModalDelete></td>
+                        </tr>)
+                    }
+                </tbody>
+            </Table>
+        </div>
     </div>
-    );
+);
 }
 
 
